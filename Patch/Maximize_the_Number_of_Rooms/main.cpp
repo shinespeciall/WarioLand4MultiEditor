d57f6930eb892cf39e6aa9f8ebb6dc9588917865
Chen Yifeng <648616375@qq.com>
2017-06-08 23:23:02 +0800

    Update main.cpp

--- Patch/Maximize_the_Number_of_Rooms/main.cpp
+++ Patch/Maximize_the_Number_of_Rooms/main.cpp
@@ -2,7 +2,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>         //exit() is in the header
-#include <string.h>
 #include <string>
 
 using namespace std;
@@ -52,10 +51,10 @@ int main(int argc,char *argv[])
 	for(i=0;i<96;i+=4)
 		pointers[i/4]=(ptrStream[i+3]<<24)+(ptrStream[i+2]<<16)+(ptrStream[i+1]<<8)+ptrStream[i]-134217728L;
 	//Then Load Room number after 639068 for each level
-	u8 Bytes[288];
+	u8 Bytes[348];
 	rewind(gbafp);
 	fseek(gbafp,6525032L,0);     //639068
-	for(i=0;i<288;i++)
+	for(i=0;i<348;i++)
 		fread(&Bytes[i],sizeof(u8),1,gbafp);
 
 	unsigned char B[24][704];        //For Save all the Bytes
@@ -72,13 +71,22 @@ int main(int argc,char *argv[])
 	printf("The following level probably can be maximized\n");
 	for(i=0;i<17;i++)                //for The 17 Level and the No.24 can be check somewhere else
 	{
-		for(j=0;j<17;++j)
+		for(j=0;j<29;++j)
 			if( ((unsigned int) Bytes[12*j]==i) & ((unsigned int) Bytes[12*j+1]<16) )
 				break;
-		if(j!=17)             //probably there is no way to judge if the Data areas of Levels have been maximized, is there?
+		if(j!=29)             //probably there is no way to judge if the Data areas of Levels have been maximized, is there?
 		{
-			printf("%4d",(unsigned int) Bytes[12*j]);     //temporary output for check
-			LevelIndex[n]=j;
+			printf("%4d",(unsigned int) Bytes[12*j]);     //output for checking
+			LevelIndex[n]=i;
+			n++;
+		}
+	}
+	for(j=0;j<29;j++)                //for the No.24
+	{
+		if( ((unsigned int) Bytes[12*j]==23) & ((unsigned int) Bytes[12*j+1]<16) )
+		{
+			printf("%4d",(unsigned int) Bytes[12*j]);
+			LevelIndex[n]=23;
 			n++;
 		}
 	}
@@ -88,14 +96,61 @@ int main(int argc,char *argv[])
 	for(i=0;i<17;++i)
 		if(LevelIndex[i]==j)
 			break;
-	if(i==16)
+	if(i==17)
 	{
 		printf("Wrong Index or don't need to maximize tha level!\tPress Enter to exit...");
 		a=getchar();
+		fclose(gbafp);
 		exit(0);
 	}
-	printf("Level%3d, start processing...",(unsigned int) LevelIndex[i]);     //temporary output for check
+	printf("Level%3d, start processing...\n",(unsigned int) LevelIndex[i]);     //output for checking
+	n=(unsigned int) LevelIndex[i];      //n=level_Index
 
+	for(j=0;j<17;++j)
+		if((unsigned int) Bytes[12*j]==n)
+			break;
+	i=(unsigned int) Bytes[12*j+1];
+
+	unsigned char New_Bytes[704];
+	for(j=0;j<(44*i);j++)
+		New_Bytes[j]=B[n][j];
+	j--;
+	for(i=44*j;i<704;i++)
+		New_Bytes[i]=(unsigned char) 241;
+	//read from 78F970 to find space
+	rewind(gbafp);
+	fseek(gbafp,7928176L,0);
+	u8 SpaceOffset[4];
+	u32 SpaceOffset2,offset2;
+	for(i=0;i<4;i++)
+		fread(&SpaceOffset[i],sizeof(u8),1,gbafp);
+	SpaceOffset2=(unsigned int) (SpaceOffset[0]<<24)+(unsigned int) (SpaceOffset[1]<<16)+(unsigned int) (SpaceOffset[2]<<8)+(unsigned int) (SpaceOffset[3]);
+	if(SpaceOffset2==4294967295)
+		SpaceOffset2=7928192;
+	if(SpaceOffset2%4!=0)
+		SpaceOffset2=(SpaceOffset2/4)*4+4;
+	offset2=SpaceOffset2;
+	SpaceOffset2=SpaceOffset2+134217728;         //add 08000000
+	//rewrite the pointer in small endian
+	rewind(gbafp);
+	fseek(gbafp,(7926400+4*n),0);
+	fwrite(&SpaceOffset2,sizeof(u32),1,gbafp);
+	//rewrite the offset in small endian
+	SpaceOffset2=SpaceOffset2+704-134217728;
+	SpaceOffset[0]=SpaceOffset2>>24;
+	SpaceOffset[1]=(SpaceOffset2<<8)>>24;
+	SpaceOffset[2]=(SpaceOffset2<<16)>>24;
+	SpaceOffset[3]=(SpaceOffset2<<24)>>24;
+	SpaceOffset2=(unsigned int) (SpaceOffset[0])+(unsigned int) (SpaceOffset[1]<<8)+(unsigned int) (SpaceOffset[2]<<16)+(unsigned int) (SpaceOffset[3]<<24);
+	rewind(gbafp);
+	fseek(gbafp,7928176L,0);   //78F970
+	fwrite(&SpaceOffset2,sizeof(u32),1,gbafp);
+	//rewrite the data area, the original area won't be cleared
+	rewind(gbafp);
+	fseek(gbafp,offset2,0);
+	for(i=0;i<704;i++)
+		fwrite(&New_Bytes[i],sizeof(u8),1,gbafp);
+	printf("Finish!");
 	fclose(gbafp);
 	printf("\nPress Enter to exit...");
 	a=getchar();
